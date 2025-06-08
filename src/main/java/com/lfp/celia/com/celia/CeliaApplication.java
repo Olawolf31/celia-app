8cc16a714c805bb488dcce362de321a567f9f331
Olawolf31 <110026445+Olawolf31@users.noreply.github.com>
2025-06-08 21:30:12 +0200

    project setup

---
+++ src/main/java/com/lfp/celia/com/celia/CeliaApplication.java
@@ -0,0 +1,13 @@
+package com.lfp.celia.com.celia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeliaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lfp/celia/com/celia/CeliaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lfp.celia.com.celia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
